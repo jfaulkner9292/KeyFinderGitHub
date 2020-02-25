c2416b93271a87231b4bb4586c819a7b9f6b9912
jfaulkner9292 <jerrison9292@gmail.com>
2020-02-24 21:24:22 -0600

    test update

--- keyfinder.c
+++ keyfinder.c
@@ -10,6 +10,8 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
 int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
             unsigned char *iv, unsigned char *plaintext);
 
+//Test edit for github
+
 int main (void)
 {
     char givenCipher[64];
